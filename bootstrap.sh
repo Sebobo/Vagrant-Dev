bfdf1547fc0c1b8e6ef1dc7b41fa071c669fe265
Sebastian Helzle <sebastian@helzle.net>
2013-04-25 08:43:49 +0200

    Installed more cookbooks. chef is upgraded before provisioning.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
-add-apt-repository ppa:chris-lea/node.js
-apt-get update
-
-# Install nodejs & npm
-apt-get install -y python-software-properties python g++ make
-apt-get install -y nodejs
+gem install chef --version 11.4.0 --no-rdoc --no-ri --conservative
