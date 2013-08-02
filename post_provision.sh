f586a788a9ee14447b9cd618ec2def3e9db464ce
Sebastian Helzle <sebastian@helzle.net>
2013-08-02 11:20:28 +0200

    Removed post provision script

--- post_provision.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Copy jenkins plugins into jenkins plugins directory
-if [ -d "/var/lib/jenkins/jenkins-data/plugins" ]; then
-  cp /vagrant/includes/jenkins-plugins/*.hpi /var/lib/jenkins/jenkins-data/plugins/
-fi
